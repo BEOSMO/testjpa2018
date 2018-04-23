7c339d0347e90e981f415fa5b53ee8d83e613688
beosmo <beosmo@github.com>
2018-04-23 14:39:49 +0200

    mise à jour des méthodes et implémentation

--- src/main/java/jpa/PersonDao.java
+++ src/main/java/jpa/PersonDao.java
@@ -3,26 +3,64 @@ package jpa;
 import domain.Person;
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
-public class PersonDao implements GenericDao<Person, Integer> {
 
+public class PersonDao implements GenericDao<Person> {
 
-    public Person create(Person person) {
-        return null;
-    }
+  private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dev");
+  private EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+  private EntityTransaction transaction = this.entityManager.getTransaction();
 
-    public Person read(Integer id, Class c) {
-        return null;
-    }
-    public List<Person> readAll() {
-        return EntityManagerHelper.getEntityManager().createQuery("select p from Person as p").getResultList();
-    }
+  /**
+   * Method to create several persons objects in the database.
+   *
+   * @param p person objects
+   */
+  public void create(Person p) {
+    this.transaction.begin();
+    this.entityManager.persist(p);
+    this.transaction.commit();
+  }
 
-    public Person update(Person person) {
-        return null;
-    }
+  /**
+   * Read provides a way to return an object from the database.
+   *
+   * @param id person id in the database
+   */
+  public Person read(int id) {
+    return this.entityManager.find(Person.class, id);
+  }
 
-    public void delete(Person person) {
+  /**
+   * Update enables us to update several objects of type person.
+   *
+   * @param p person objects
+   */
+  public void update(Person p) {
+    this.transaction.begin();
+    this.entityManager.merge(p);
+    this.transaction.commit();
+  }
 
-    }
+  /**
+   * This method deletes several person objects records from the database.d
+   *
+   * @param o person objects
+   */
+  public void delete(Person o) {
+    this.transaction.begin();
+    this.entityManager.remove(o);
+    entityManager.flush();
+    entityManager.clear();
+    this.transaction.commit();
+  }
+
+  public List<Person> readAll() {
+    return this.entityManager.createQuery("from Person", Person.class).getResultList();
+  }
+
 }
